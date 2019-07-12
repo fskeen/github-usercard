d4b1adc0e71277047fbc40f81664dc0f169f9d8f
Faye Skeen <fskeen@gmail.com>
2019-07-11 17:10:56 -0700

    Got followers to show up programmatically. Now to get their data to populate correctly.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,8 +2,15 @@
            (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
-const promise = axios.get(`https://api.github.com/users/fskeen`)
-console.log(promise)
+const userData = axios.get(`https://api.github.com/users/fskeen`)
+userData
+  .then(a => {
+    const dataDiv = createGithubCard(a)
+    document.querySelector(".cards").appendChild(dataDiv)
+  })
+  .catch(error => {
+    console.log(`Hmmm, not quite right. Check this out: ${error}`)
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -25,7 +32,46 @@ console.log(promise)
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+let followersArray = [];
+const followers = axios.get("https://api.github.com/users/fskeen/followers")
+
+followers
+  .then(a => {
+    // console.log("Initial return from API call: ", a);
+
+    // iterate through the array of followers listed in the data section of the JSON
+    a.data.map(follower => {
+      // console.log("follower data", follower)
+
+      // capture each individual follower data in an object so that it matches the format expected by the constructor function (object.data.name, etc.)
+      let followObj = {data: follower}
+      // console.log("followObj var in fx", followObj)
+
+      // push the object to the array
+      followersArray.push(followObj)
+      // console.log("objs pushed to array", followersArray)
+      return followersArray
+    });
+
+    // console.log("followersArray after fx", followersArray)
+
+    // run the constructor function for each object in the array & store value
+    followersArray.map(arr => {
+      document.querySelector(".cards").appendChild(createGithubCard(arr))
+    })
+
+    // console.log("followersData", followersData)
+    // append those values to the page
+    
+
+  })
+  .catch(error => {
+    console.log(`Hmmm, wrong. Check this out: ${error}`)
+  })
+  console.log("followersArray variable: ", followersArray)
+
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -46,43 +92,47 @@ const followersArray = [];
 </div>
 */
 
-function createGithubCard (obj) {}
+function createGithubCard (obj) {
 
-// creating the elements of the page
-function createElement(name, className = null, content = '', setAttribute = null, attributeValue  = null) {
-  if(!name) return;
-  let element = document.createElement(name);
-  if(className) element.classList.add(className);
-  element.textContent = content;
-  if(setAttribute) element.setAttribute(`${setAttribute}`, `${attributeValue}`);
-  return element;
+  // creating the elements of the page
+  function createElement(name, className = null, content = '', setAttribute = null, attributeValue  = null) {
+    if(!name) return;
+    let element = document.createElement(name);
+    if(className) element.classList.add(className);
+    element.textContent = content;
+    if(setAttribute) element.setAttribute(`${setAttribute}`, `${attributeValue}`);
+    return element;
+  }
+
+  let cardContainer = createElement("div", "card", undefined);
+  let cardImg = createElement("img", undefined, undefined, "src", obj.data.avatar_url);
+  let cardInfo = createElement("div", "card-info", undefined);
+  let cardName = createElement("h3", "name", obj.data.name);
+  let cardUsername = createElement("h3", "username", obj.data.login);
+  let cardLocation = createElement("p", undefined, obj.data.location)
+  let cardProfile = createElement("p", undefined, `Profile: `)
+  let cardProfileLink = createElement("a", undefined, obj.data.html_url, "src", "https://api.github.com/users/fskeen")
+  let cardFollowers = createElement("p", undefined, `Followers: ${obj.data.followers}`)
+  let cardFollowing = createElement("p", undefined, `Following: ${obj.data.following}`)
+  let cardBio = createElement("p", undefined, obj.data.bio)
+
+  // sewing them together into a layout
+
+  HTMLElement.prototype.appendChildren = function () {
+    for (let i = 0; i < arguments.length ; i++) {
+      this.appendChild(arguments[i])
+    }
+  };
+
+  cardContainer.appendChildren(cardImg, cardInfo);
+  cardInfo.appendChildren(cardName, cardUsername, cardLocation, cardProfile, cardFollowers, cardFollowing, cardBio);
+  cardProfile.appendChildren(cardProfileLink);
+
+  return cardContainer
 }
 
-let cardContainer = createElement("div", "card", undefined);
-let cardImg = createElement("img", undefined, undefined, "src", "https://avatars1.githubusercontent.com/u/50350459?v=4");
-let cardInfo = createElement("div", "card-info", undefined);
-let cardName = createElement("h3", "name", "Bob");
-let cardUsername = createElement("h3", "username", "username here");
-let cardLocation = createElement("p", undefined, "location here")
-let cardProfile = createElement("p", undefined, "Profile: ")
-let cardProfileLink = createElement("a", undefined, "Find this person on Github here.", "src", "https://api.github.com/users/fskeen")
-let cardFollowers = createElement("p", undefined, "Who this person follows")
-let cardFollowing = createElement("p", undefined, "Who follows this person")
-let cardBio = createElement("p", undefined, "User Bio here")
 
-// sewing them together into a layout
-
-HTMLElement.prototype.appendChildren = function () {
-  for (let i = 0; i < arguments.length ; i++) {
-    this.appendChild(arguments[i])
-  }
-};
-
-cardContainer.appendChildren(cardImg, cardInfo);
-cardInfo.appendChildren(cardName, cardUsername, cardLocation, cardProfile, cardFollowers, cardFollowing, cardBio);
-cardProfile.appendChildren(cardProfileLink);
-
-console.log(cardContainer)
+// console.log(userData)
 
 
 
